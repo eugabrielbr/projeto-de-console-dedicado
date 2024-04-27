e3898075b4534a3c575bfa2442402b5fb2698777
duzzes <diogosj2003@gmail.com>
2024-04-27 20:07:04 -0300

    :feat: Adicionado destaque à combinação vencedora ao fim do jogo.

--- Main.c
+++ Main.c
@@ -41,18 +41,61 @@ void print_tabela_colorida(char tabela[3][3], int lin, int col){
     printf("\n");
 }
 
-int check_winner(const char tabela[3][3]){
+void print_tabela_resultado(char tabela[3][3], int *lin1, int *col1, int *lin2, int *col2, int *lin3, int *col3){
+    printf("\n");
+    for(int i = 0; i < 3; i++) {
+      if((*lin1 == i && *col1 == 0) || (*lin2 == i && *col2 == 0) || (*lin3 == i && *col3 == 0)) printf(verde"  %c  "padrao, tabela[i][0]);
+      else printf("  %c  ", tabela[i][0]);
+      printf("║");
+      if((*lin1 == i && *col1 == 1) || (*lin2 == i && *col2 == 1) || (*lin3 == i && *col3 == 1)) printf(verde"  %c  "padrao,tabela[i][1]);
+      else printf("  %c  ",tabela[i][1]);
+      printf("║");
+      if((*lin1 == i && *col1 == 2) || (*lin2 == i && *col2 == 2) || (*lin3 == i && *col3 == 2)) printf(verde"  %c  "padrao,tabela[i][2]);
+      else printf("  %c  ",tabela[i][2]);
+      if(i < 2) printf("\n═════╬═════╬═════\n");
+    }
+    printf("\n");
+
+
+
+}
+
+int check_winner(const char tabela[3][3], int *lin1, int *col1, int *lin2, int *col2, int *lin3, int *col3){
     if(tabela[0][0] == tabela[1][1] && tabela[1][1] == tabela[2][2] && tabela[0][0] != ' '){
+      *lin1 = 0;
+      *col1 = 0;
+      *lin2 = 1;
+      *col2 = 1;
+      *lin3 = 2;
+      *col3 = 2;
       return 1;
     }
     if(tabela[0][2] == tabela[1][1] && tabela[1][1] == tabela[2][0] && tabela[0][2] != ' '){
+      *lin1 = 0;
+      *col1 = 2;
+      *lin2 = 1;
+      *col2 = 1;
+      *lin3 = 2;
+      *col3 = 0;
       return 1;
     }
     for(int i = 0; i < 3; i++){
       if(tabela[0][i] == tabela[1][i] && tabela[1][i] == tabela[2][i] && tabela[0][i] != ' '){
+        *lin1 = 0;
+        *col1 = i;
+        *lin2 = 1;
+        *col2 = i;
+        *lin3 = 2;
+        *col3 = i;
         return 1;
       }
       if(tabela[i][0] == tabela[i][1] && tabela[i][1] == tabela[i][2] && tabela[i][0] != ' '){
+        *lin1 = i;
+        *col1 = 0;
+        *lin2 = i;
+        *col2 = 1;
+        *lin3 = i;
+        *col3 = 2;
         return 1;
       }
     }
@@ -163,10 +206,10 @@ void calcular_quadrante(int *quadrante, int lin, int col){
     else if (lin == 2) *quadrante = lin + col + 5;
 }
 
-int finalizar_jogo(int cont, char tabela[3][3], int player){
-    if(check_winner(tabela)){
+int finalizar_jogo(int cont, char tabela[3][3], int player, int *lin1, int *col1, int *lin2, int *col2, int *lin3, int *col3){
+    if(check_winner(tabela, lin1, col1, lin2, col2, lin3, col3)){
       system("clear");
-      print_tabela(tabela);
+      print_tabela_resultado(tabela, lin1, col1, lin2, col2, lin3, col3);
       printf("Vitória jogador %d\n", player);
       return 1;
     }
@@ -199,6 +242,7 @@ int main(void) {
     int sensi_movx = 0;
     int sensi_movy = 0;
     int v1,v2 = 1;
+    int lin1, col1, lin2, col2, lin3, col3;
     while(1){
       calcular_quadrante(&quadrante, lin, col);
       if((v1 || v2) || (cont == 0)){
@@ -211,7 +255,7 @@ int main(void) {
       botao2 = (int)mouse_data[3];
       v1 = clique_mouse(botao, botao2, tabela, mouse_data, &cont, player, lin, col);
       v2 = movimentacao_mouse(&lin, &col, mov_x, mov_y, &sensi_movx, &sensi_movy);
-      if(finalizar_jogo(cont, tabela, player)){
+      if(finalizar_jogo(cont, tabela, player, &lin1, &col1, &lin2, &col2, &lin3, &col3)){
         break;
       }
       if(cont % 2 == 0){
